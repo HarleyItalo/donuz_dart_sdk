04f069d79df14a3a446b4b3ce971b11df9b75b8f
Harley Italo Soares Xavier <harleyitalo@hotmail.com>
2023-05-04 17:07:20 -0300

    adicionando o modulo de prizes.

--- lib/donuz_dart_sdk.dart
+++ lib/donuz_dart_sdk.dart
@@ -1,6 +1,7 @@
 library donuz_dart_sdk;
 
 import 'package:donuz_dart_sdk/modules/establishment/establishment_module.dart';
+import 'package:donuz_dart_sdk/modules/prizes/prizes_module.dart';
 import 'package:donuz_dart_sdk/modules/user/user_module.dart';
 import 'package:http/http.dart' as http;
 import 'package:donuz_dart_sdk/modules/common/common_module.dart';
@@ -9,13 +10,16 @@ class DonuzSDK {
   final _serviceLocator = GetIt.instance;
   late EstablismentModule establisment;
   late UserModule user;
+  late PrizeModule prizes;
   DonuzSDK({required BaseConfig config, required http.Client client}) {
     CommomModule(_serviceLocator, config, client);
     establisment = EstablismentModule(instance: _serviceLocator);
     user = UserModule(instance: _serviceLocator);
+    prizes = PrizeModule(instance: _serviceLocator);
   }
   init() async {
     await establisment.init();
     await user.init();
+    await prizes.init();
   }
 }
--- lib/modules/prizes/prizes_module.dart
+++ lib/modules/prizes/prizes_module.dart
@@ -1,6 +1,34 @@
 //models
+import 'package:donuz_dart_sdk/modules/base_module.dart';
+import 'package:donuz_dart_sdk/modules/prizes/prizes_module.dart';
+
 export 'models/prize_model.dart';
 //repositories
 export 'repositories/prize_repository.dart';
 // usercases
 export 'usercases/get_prizes.dart';
+
+class PrizeModule extends BaseModule {
+  PrizeModule({required super.instance});
+
+  late GetPrizes getPrizes;
+  @override
+  Future init() async {
+    getPrizes = await instance.getAsync<GetPrizes>();
+  }
+
+  @override
+  Future injectModule() async {
+    instance.registerLazySingletonAsync<GetPrizes>(
+      () async => GetPrizesImpl(
+        await instance.getAsync(),
+        await instance.getAsync(),
+      ),
+    );
+    instance.registerLazySingletonAsync<PrizeRepository>(
+      () async => PrizeRepositoryImpl(
+        await instance.getAsync(),
+      ),
+    );
+  }
+}
