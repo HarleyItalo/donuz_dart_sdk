35d8ac4879d9ab533575cfffc1d8cd7a1cc1fd8f
Harley Italo Soares Xavier <harleyitalo@hotmail.com>
2023-05-31 23:26:12 -0300

    Removendo imports desnecessarios

--- test/modules/prizes/usercases/get_prizes_categories_test.dart
+++ test/modules/prizes/usercases/get_prizes_categories_test.dart
@@ -1,11 +1,8 @@
 import 'dart:convert';
 import 'package:donuz_dart_sdk/modules/prizes/models/prize_category_model.dart';
-import 'package:donuz_dart_sdk/modules/prizes/models/prize_model.dart';
-import 'package:donuz_dart_sdk/modules/prizes/usercases/get_prize_by_id.dart';
 import 'package:donuz_dart_sdk/modules/prizes/usercases/get_prize_categories.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import '../../../json/prizes/get_prize_by_id_json.dart';
 import '../../../json/prizes/prizes_categories_json.dart';
 import '../../../mocks/establishment/usercases/find_establishment_by_id_mock.dart';
 import '../../../mocks/prizes/repositories/prizes_repository_mock.dart';
