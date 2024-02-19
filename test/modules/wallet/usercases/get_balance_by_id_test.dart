65af50a6b7e3cdc5a60d91d601529a2e87f6e18c
Harley Italo Soares Xavier <harleyitalo@hotmail.com>
2024-02-19 10:50:48 -0300

    remove imports unused.

--- test/modules/wallet/usercases/get_balance_by_id_test.dart
+++ test/modules/wallet/usercases/get_balance_by_id_test.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:donuz_dart_sdk/modules/establishment/establishment_module.dart';
-import 'package:donuz_dart_sdk/modules/user/user_module.dart';
 import 'package:donuz_dart_sdk/modules/wallet/usercases/get_balance_by_id.dart';
 import 'package:donuz_dart_sdk/modules/wallet/wallet_module.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,13 +8,11 @@ import 'package:mocktail/mocktail.dart';
 
 import '../../../json/wallet/balance_json.dart';
 import '../../../mocks/establishment/usercases/find_establishment_by_id_mock.dart';
-import '../../../mocks/user/user_cases/get_logged_user_token_mock.dart';
 import '../../../mocks/wallet/repositories/point_respository_mock.dart';
 
 void main() {
   final PointsRepository repositoryMock = PointsRepositoryMock();
   FindEstablishmentById findEstablismentByIdMock = FindEstablismentByIdMock();
-  GetLoggedUserToken getLoggedUserToken = GetLoggedUserTokenMock();
   GetBalanceById getBalance =
       GetBalanceByIdImpl(repositoryMock, findEstablismentByIdMock);
 
